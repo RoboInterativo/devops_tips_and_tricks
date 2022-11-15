2041693188688fa3124ef45a33b4706361592f31
chiefexb <chief_exb@mail.ru>
2022-11-15 15:33:47 +0300

    1

--- gitlab/gitlab-cli.sh
+++ gitlab/gitlab-cli.sh
@@ -19,17 +19,22 @@ curl -v --request POST --header "PRIVATE-TOKEN: ${TOKEN}" \
 
 function create_projects() {
   local project_name=${1}
-  local project_path=${2}
-  local group_id=${3}
+  local group_id=${2}
   if [ -z "${project_name}" ]; then
     echo "* Parameter PROJECT_NAME is missing" >&2
     display_usage
     exit 1
   fi
+  if [ -z "${group_id}" ]; then
+    echo "* Parameter GROUP is missing" >&2
+    name_space_block=""
+  else
+      name_space_block='"namespace_id": namespace_id_p , '
+  fi
 
-echo "Project_name="$project_name", project path="$project_path
+echo "Project_name="$project_name", group_id="$group_id
 
-data=`echo '{ "name": "project_name", "description": "New Project",    "initialize_with_readme": "true"}' | sed 's/project_name/'$1'/g'`
+data=`echo '{ "name": "project_name", '$name_space_block'"description": "New Project",    "initialize_with_readme": "true"}' | sed 's/project_name/'$1'/g'|sed 's/namespace_id_p/'$group_id'/g' `
 
 url=$base_url'/projects'
 curl --request POST --header "PRIVATE-TOKEN: ${TOKEN}" \
@@ -52,11 +57,8 @@ while [[ $# -gt 0 ]]; do
     case "${param}" in
       --create-project)
       p_project_name="$1"
-      shift
-      # p_project_path="$1"
-      # shift
-
       action=createProject
+      shift
       ;;
       --group_id)
       p_group_id="$1"
@@ -68,7 +70,7 @@ case "${action}" in
   createProject)
       echo $p_project_name  $p_project_path $group_id
 
-      create_projects    "${p_project_name}" "${param_group_id}"
+      create_projects    "${p_project_name}" "${p_group_id}"
       ;;
   esac
 
